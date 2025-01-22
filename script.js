1e224327ddaf0cd66792a6e431a60370cbf0b9ea
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-22 11:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function promises(){
+            const name = document.getElementById('name').value;
+            const age = document.getElementById('age').value;
+            new Promise ((resolve,reject)=>{
+                if(age>18){
+                    resolve(name);
+                }else{
+                    reject(name);
+                }
+            })
+            .then((name)=> alert(`Welcome, ${name} You can vote.`))
+            .catch((name)=> alert(`Oh sorry ${name} You aren't old enough.`));
+        }
